1da3903927c93c2ee7e631b3320627d14cc26355
neelgadhiya <128478060+neelgadhiya@users.noreply.github.com>
2024-03-02 15:55:55 +0530

    Add files via upload

---
+++ src/Nav.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css"
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    };
+  
+    window.addEventListener('scroll', handleScroll);
+  
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Netflix_Logomark.png/640px-Netflix_Logomark.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/84c20033850498.56ba69ac290ea.png"
+        alt="Netflix Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
